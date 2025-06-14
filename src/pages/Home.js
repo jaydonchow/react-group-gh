daac5ae8b4a55868854cc5c65063e236ec278bb9
zoujiajin <zoujiajin@skieer.com>
2025-06-14 21:22:18 +0800

    page init

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ import { SetOutline, AddOutline } from "antd-mobile-icons";
 
 import AddDataForm from "@/component/AddDataForm";
 import TimelineContent from "@/component/TimelineContent";
-import thImage from "@/assets/th.jpg";
-import seaImage from "@/assets/sea.jpg";
-import birdImage from "@/assets/bird.jpg";
-import lionImage from "@/assets/lion.jpg";
+// import thImage from "@/assets/th.jpg";
+// import seaImage from "@/assets/sea.jpg";
+// import birdImage from "@/assets/bird.jpg";
+// import lionImage from "@/assets/lion.jpg";
 import { addNoteItem, queryByUserId, updateNoteItem, deleteNoteItem, getUserProfile } from "../api/index";
 import Header from "@/component/Header";
 export default () => {
@@ -24,52 +24,52 @@ export default () => {
       if (isFirstLoad === 0) {
         setIsFirstLoad(1);
       }
-      console.log("fetchData", res);
-      res = [
-        {
-          description: "今天生日，一起吃烤肉!!",
-          dateValue: "2025-05-13",
-          title: "",
-          userId: "1",
-          fileList: [
-            {
-              url: thImage,
-            },
-            {
-              url: seaImage,
-            },
-            {
-              url: birdImage,
-            },
-          ],
-          id: "68280568b9570274df1a7654",
-        },
-        {
-          description: "云南之旅",
-          dateValue: "2025-05-01",
-          userId: "1",
-          fileList: [
-            {
-              url: seaImage,
-            },
-            {
-              url: lionImage,
-            },
-          ],
-          id: "682b582bd3496c4246834e09",
-        },
-        {
-          description: "开心开心",
-          dateValue: "2025-06-12",
-          userId: "1",
-          fileList: [
-            {
-              url: birdImage,
-            },
-          ],
-          id: "684a9f51bf6625518c5a4e21",
-        },
-      ];
+      // console.log("fetchData", res);
+      // res = [
+      //   {
+      //     description: "今天生日，一起吃烤肉!!",
+      //     dateValue: "2025-05-13",
+      //     title: "",
+      //     userId: "1",
+      //     fileList: [
+      //       {
+      //         url: thImage,
+      //       },
+      //       {
+      //         url: seaImage,
+      //       },
+      //       {
+      //         url: birdImage,
+      //       },
+      //     ],
+      //     id: "68280568b9570274df1a7654",
+      //   },
+      //   {
+      //     description: "云南之旅",
+      //     dateValue: "2025-05-01",
+      //     userId: "1",
+      //     fileList: [
+      //       {
+      //         url: seaImage,
+      //       },
+      //       {
+      //         url: lionImage,
+      //       },
+      //     ],
+      //     id: "682b582bd3496c4246834e09",
+      //   },
+      //   {
+      //     description: "开心开心",
+      //     dateValue: "2025-06-12",
+      //     userId: "1",
+      //     fileList: [
+      //       {
+      //         url: birdImage,
+      //       },
+      //     ],
+      //     id: "684a9f51bf6625518c5a4e21",
+      //   },
+      // ];
       setIsFirstLoad(2);
       setDataList(res);
     });
